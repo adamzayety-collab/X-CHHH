3f85e07a8c2a5f638f6450b0253d114026f96d9f
adamzayety-collab <adamzayety@gmail.com>
2025-12-29 06:39:33 -0800

    Add files via upload

---
+++ Temp Cleaner.bat
@@ -0,0 +1,10 @@
+@echo off
+@echo Made By Sniper
+@echo.
+color 3
+Del /S /F /Q %temp%
+Del /S /F /Q %Windir%\Temp
+Del /S /F /Q C:\WINDOWS\Prefetch
+@echo.
+@echo All Temporary Files Sucessfully Deleted!
+@echo. 
