343abdeefebe7f84b9dc1c98e8ca30cafc7312f6
adamzayety-collab <adamzayety@gmail.com>
2025-12-29 14:32:12 -0800

    Add files via upload

---
+++ fix_internet.bat
@@ -0,0 +1,10 @@
+ÿþ&cls
+ÿþ
+::                                                            by Sniper
+
+netsh advfirewall firewall delete rule name=all program="C:\Program Files\BlueStacks_nxt\HD-Player.exe"
+netsh advfirewall firewall delete rule name=all program="C:\Program Files\BlueStacks_msi5\HD-Player.exe"
+netsh advfirewall firewall delete rule name=all program="C:\Program Files\BlueStacks\HD-Player.exe"
+netsh advfirewall firewall delete rule name=all program="C:\Program Files\BlueStacks_msi2\HD-Player.exe"
+netsh advfirewall firewall delete rule name=all program="C:\Program Files (x86)\SmartGaGa\ProjectTitan\Engine\ProjectTitan.exe"
+::                                                            by Sniper
